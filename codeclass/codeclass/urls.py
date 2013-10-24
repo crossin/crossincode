b4be97573f87237b00d7fdea501143a52ceab562
Crossin <roundstars@163.com>
2013-10-24 13:45:03 +0800

    fix register bug

--- codeclass/codeclass/urls.py
+++ codeclass/codeclass/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from registration import views as reg_views, forms as reg_forms
+from registration.backends.default.views import RegistrationView
+from registration.forms import RegistrationFormUniqueEmail
+
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/register/$', reg_views.RegistrationView.as_view(
-        form_class=reg_forms.RegistrationFormUniqueEmail),
+    url(r'^accounts/register/$',
+        RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),
         name='registration_register'),
     url(r'^accounts/', include('registration.backends.default.urls')),
 
