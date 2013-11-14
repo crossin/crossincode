b2d0279a1f30cd7c227cf10209503e1cba8d3e3d
Crossin <roundstars@163.com>
2013-11-14 14:16:00 +0800

    code ide on wechat

--- codeclass/oj/urls.py
+++ codeclass/oj/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+
+from oj import views
 
 
 dajaxice_autodiscover()
@@ -8,4 +9,6 @@ dajaxice_autodiscover()
 urlpatterns = patterns(
     '',
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+    url(r'^wechat/$', views.wechat_view),
+    url(r'^output/$', views.output_view),
 )
--- codeclass/oj/views.py
+++ codeclass/oj/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.template.response import TemplateResponse
+
+from runcode import run
+
+
+def wechat_view(request):
+    return TemplateResponse(request, 'oj/wechat.html', {})
+
+
+def output_view(request):
+    if request.method == 'POST':
+        code = request.POST.get('code')
+        result = run(code, '')
+        return TemplateResponse(
+            request,
+            'oj/output.html',
+            {
+                'code': code,
+                'result': result[0],
+            })
+    return HttpResponseRedirect(reverse(wechat_view))
